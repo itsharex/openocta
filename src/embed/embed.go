64eb80a71649720a79dc5a754c8aab22b7b1f607
zhanbei <zhanbei@databuff.com>
2026-03-20 17:20:53 +0800

    feature 删除无关紧要的构建中间产物文件

--- src/embed/embed.go
+++ src/embed/embed.go
@@ -12,7 +12,7 @@ import (
 
 // Assets embeds frontend, config-schema, openocta.json.example, .env.
 //
-//go:embed frontend openocta.json.example .env
+//go:embed frontend .env
 var assets embed.FS
 
 func init() {
